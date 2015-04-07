abe54bdc554f6cc98ecf09f836c8e70ee987220e
Sameer Rahmani <lxsameer@gnu.org>
2015-04-07 23:39:59 +0430

    fontend basic rtl form added

---
+++ app/overrides/rtl_frontend_html.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(virtual_path: 'spree/layouts/spree_application.html.erb',
+                     add_to_attributes: 'html[lang=fa]',
+                     attributes: { dir: 'rtl' },
+                     name: 'rtl_layout')
